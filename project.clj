72ccb550279b9363bc5b5316eca1f3fd889203a4
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-05-12 16:49:30 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.exoscale/vinyl "0.1.0-SNAPSHOT"
+(defproject com.exoscale/vinyl "0.1.0"
   :description "Clojure facade for the FoundationDB record-layer"
   :url "https://github.com/exoscale/vinyl"
   :license {:name "MIT/ISC"}
