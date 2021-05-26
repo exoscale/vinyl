19c18fe2caa188f9c66f5f789565fba84a483400
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-05-26 14:27:26 +0200

    add simplified SQL facade
    Queries take the following form:
    
           SELECT * FROM <record-type> <filters> <options>
    
    No field selection can be done, queries **must** start with 'SELECT *'.
    Record types are not checked at query parsing time.
    
    Supported filters:
    
    - field comparisons: field = value, field != value, field starts with 'prefix'
    - field presence test: field is null, field is not null
    - field set membership: field in ( 1, 2, 3)
    - integer comparisons: field > val, field < val, field >= val, field <= val
    - logical operations: field = v1 or vield = v2 and state = 'disabled'
    - grouping: (field = v1 or field = v2)
    - group negation: !(field = v1 or field = v2)
    
    All value filters work on nested fields:
    
        parent.child = 'foo'
    
    Trailing options can be any of:
    - LIMIT <number>
    - SKIP <number>

--- project.clj
+++ project.clj
@@ -8,17 +8,18 @@
                  [org.foundationdb/fdb-record-layer-core-pb3 "2.8.110.0"]
                  [com.google.protobuf/protobuf-java          "3.15.8"]
                  [com.stuartsierra/component                 "1.0.0"]
-                 [exoscale/ex                                "0.3.17"]]
+                 [exoscale/ex                                "0.3.17"]
+                 [instaparse                                 "1.4.10"]]
 
   :deploy-repositories [["snapshots" :clojars]
                         ["releases"  :clojars]]
 
   :global-vars {*warn-on-reflection* true}
-  :java-source-paths ["protobuf"]
   :pedantic? :warn
   :profiles
-  {:dev  {:dependencies [[org.slf4j/slf4j-api    "1.7.30"]
-                         [org.slf4j/slf4j-simple "1.7.30"]
-                         [org.clojure/test.check "1.1.0"]]}
+  {:dev  {:dependencies      [[org.slf4j/slf4j-api    "1.7.30"]
+                              [org.slf4j/slf4j-simple "1.7.30"]
+                              [org.clojure/test.check "1.1.0"]]
+          :java-source-paths ["protobuf"]}
    :lint {:pedantic? :ranges
           :plugins   [[lein-cljfmt "0.7.0"]]}})
---
+++ test/exoscale/vinyl/sql_test.clj
@@ -0,0 +1,30 @@
+(ns exoscale.vinyl.sql-test
+  (:require [clojure.test              :as test :refer [deftest are]]
+            [exoscale.vinyl.payload    :as p]
+            [exoscale.vinyl.sql        :refer [list-query]]
+            [exoscale.vinyl.demostore  :as ds :refer [*db*]]))
+
+(test/use-fixtures :once ds/with-open-fdb)
+
+(deftest query-test
+  (let [opts {:exoscale.vinyl.store/transform p/parse-record}]
+    (are [query results] (= results @(list-query *db* query opts))
+      "SELECT * FROM User WHERE name STARTS WITH 'a1'"
+      [{:id 1 :account-id 1 :name "a1u1" :email "a1u1@hello.com"}
+       {:id 2 :account-id 1 :name "a1u2" :email "a1u2@hello.com"}]
+
+      "SELECT * FROM Invoice where id >= 3"
+      [{:id 3 :account-id 3 :total 80 :lines [{:product "p4" :quantity 1}]}
+       {:id 4 :account-id 4 :total 10 :lines [{:product "p1" :quantity 2}
+                                              {:product "p2" :quantity 4}]}
+       {:id 5 :account-id 4 :total 80 :lines [{:product "p4" :quantity 1}]}]
+
+      "SELECT * FROM Account where state != 'terminated'"
+      [{:id 1 :name "a1" :state :active}
+       {:id 2 :name "a2" :state :suspended}
+       {:id 3 :name "a3" :state :suspended}]
+
+      "SELECT * FROM Account where !(state = 'terminated')"
+      [{:id 1 :name "a1" :state :active}
+       {:id 2 :name "a2" :state :suspended}
+       {:id 3 :name "a3" :state :suspended}])))
