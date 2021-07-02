87e45d6ae94dbe8896eeb0df83d7a2e537d184fe
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-07-02 15:48:14 +0200

    store: allow configurable scan properties in scan-range
    Large range scans may happen in the record layer by either
    scanning explicit ranges or execute queries for which the
    plan determines a scan should be performed. While this is not
    something one should optimize for, it is legitimate to want
    to perform large scans on occasion.
    
    In this case, it may be ok to allow relaxing consistency
    guarantees. To achieve that, we introduce a low-level
    `continuation-traversing-reducer` function, used by
    `long-range-reducer` and `long-query-reducer`.
    
    A lot of help from @arnaudgeiser, especially around fixing
    prefix range building.

--- src/exoscale/vinyl/fn.clj
+++ src/exoscale/vinyl/fn.clj
@@ -1,13 +1,52 @@
 (ns exoscale.vinyl.fn
   "Utility functions to build java.util.function implementations"
   (:import java.util.function.Function
-           java.util.function.BiFunction
-           java.util.function.Consumer))
+           java.util.function.BiConsumer
+           java.util.function.Supplier
+           java.util.concurrent.CompletableFuture
+           java.lang.AutoCloseable))
 
-(defn ^Function make-fun
-  "Turn a function of one argument into a java Function"
+;; Add an interface that extends everything reifed
+;; by `make-fun`
+(gen-interface
+ :name exoscale.vinyl.fn.Functional
+ :extends [java.util.function.Function
+           java.util.function.BiFunction
+           java.util.function.Consumer
+           java.util.function.BiConsumer])
+
+(defn ^exoscale.vinyl.fn.Functional make-fun
+  "Turn a function of one argument into an implementation
+   of `Function`, `BiFunction`, `Consumer`, and `BiConsumer`.
+
+   To allow type hinting on the way out, the extending interface
+   `exoscale.vinyl.fn.Functional` is used which extends all
+    of the above."
+  [f]
+  (if (instance? Function f)
+    f
+    (reify
+      exoscale.vinyl.fn.Functional
+      (apply  [_ arg]       (f arg))
+      (apply  [_ arg1 arg2] (f arg1 arg2))
+      (accept [_ arg]       (f arg))
+      (accept [_ arg1 arg2] (f arg1 arg2)))))
+
+(defn ^Supplier make-supplier
   [f]
   (reify
-    Function   (apply  [_ arg]       (f arg))
-    BiFunction (apply  [_ arg1 arg2] (f arg1 arg2))
-    Consumer   (accept [_ arg]       (f arg))))
+    Supplier (get [_] (f))))
+
+(defmacro supplier
+  [& fntail]
+  `(make-supplier (fn [] ~@fntail)))
+
+(defn when-complete
+  [^CompletableFuture x f]
+  (.whenComplete x (reify BiConsumer (accept [_ v t] (f v t)))))
+
+(defn close-on-complete
+  [^CompletableFuture x opened]
+  (.whenComplete
+   x
+   (reify BiConsumer (accept [_ _ _] (.close ^AutoCloseable opened)))))
