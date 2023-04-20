0ff412406dec511fdea62518f512b7ce12df1af0
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2023-04-20 22:03:18 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! Improve

--- test/exoscale/vinyl/store_test.clj
+++ test/exoscale/vinyl/store_test.clj
@@ -156,39 +156,55 @@
       (store/save-record-batch new-ds-writer (ds/all-records))
       (is @(store/list-query old-ds-reader [:User [:starts-with? :name "a1"]])))))
 
+(defn- refcounting-frequencies
+  ([] (refcounting-frequencies #{}))
+  ([removed]
+   (let [fixtures (remove (fn [{:keys [id]}] (removed id)) (:Invoice ds/fixtures))]
+     (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) fixtures)))))
+
+(defn- scan-refcounting-index
+  ([] (scan-refcounting-index tuple/all))
+  ([range]
+   (let [entries @(store/scan-index *db* "refcount_index" ::store/by-group
+                                    range nil {::store/list? true})]
+     (into {}
+           (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) entries)))))
+
+(deftest reindex-test
+  (is (= (refcounting-frequencies)
+         (scan-refcounting-index)))
+
+  (store/reindex *db* "refcount_index")
+  (store/reindex *db* "refcount_index")
+
+  (is (= (refcounting-frequencies)
+         (scan-refcounting-index))))
+
 (deftest scan-index-test
   (store/insert-record *db* (p/object->record {:bucket "bucket" :path "path" :size 2}))
-  (store/reindex *db* "refcount_index")
-  (store/reindex *db* "refcount_index")
-  (is
-   (= (assoc (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) (:Invoice ds/fixtures)))
-             "bucket" 1)
-      (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group tuple/all nil {::store/list? true})))))
-  (is
-   (= (assoc (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) (:Invoice ds/fixtures)))
-             "bucket" 1)
-      (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group tuple/all nil {::store/list? true})))))
+
+  (is (= (assoc (refcounting-frequencies) "bucket" 1)
+         (scan-refcounting-index)))
   (store/delete-record *db* :Invoice [1 1])
-  (is
-   (= (assoc (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) (remove (fn [{:keys [id]}] (= id 1)) (:Invoice ds/fixtures))))
-             "bucket" 1)
-      (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group tuple/all nil {::store/list? true})))))
+  (is (= (assoc (refcounting-frequencies #{1}) "bucket" 1)
+         (scan-refcounting-index)))
   (store/delete-record *db* :Invoice [1 2])
-  (is
-   (= (assoc (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) (remove (fn [{:keys [id]}] (#{1 2} id)) (:Invoice ds/fixtures))))
-             "bucket" 1)
-      (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group tuple/all nil {::store/list? true})))))
+  (is (= (assoc (refcounting-frequencies #{1 2}) "bucket" 1)
+         (scan-refcounting-index)))
   (store/delete-record *db* :Invoice [3 3])
-  (is
-   (= (assoc (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) (remove (fn [{:keys [id]}] (#{1 2 3} id)) (:Invoice ds/fixtures))))
-             "bucket" 1)
-      (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group tuple/all nil {::store/list? true})))))
+  (is (= (assoc (refcounting-frequencies #{1 2 3}) "bucket" 1)
+         (scan-refcounting-index)))
   (store/delete-record *db* :Invoice [4 4])
-  (is (= #{"p1" "p2"} (into #{} (map #(second (.getKey %)) @(store/scan-index *db* "refcount_index" ::store/by-group (tuple/all-of ["zero"]) nil {::store/list? true})))))
-  (is
-   (= (assoc (frequencies (mapcat #(map (fn [line] (:product line)) (:lines %)) (remove (fn [{:keys [id]}] (#{1 2 3 4} id)) (:Invoice ds/fixtures))))
-             "bucket" 1)
-      (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group (tuple/all-of ["refcount"]) nil {::store/list? true})))))
+  (is (= (assoc (refcounting-frequencies #{1 2 3 4}) "bucket" 1)
+         (scan-refcounting-index (tuple/all-of ["refcount"]))))
+  (is (= #{"p1" "p2"}
+         (into #{} (map #(second (.getKey %))
+                        @(store/scan-index *db* "refcount_index" ::store/by-group
+                                           (tuple/all-of ["zero"]) nil {::store/list? true})))))
   (store/delete-record *db* :Invoice [4 5])
-  (is (= #{"p1" "p2" "p4"} (into #{} (map #(second (.getKey %)) @(store/scan-index *db* "refcount_index" ::store/by-group (tuple/all-of ["zero"]) nil {::store/list? true})))))
-  (is (= {"bucket" 1} (into {} (map (juxt #(second (.getKey %)) #(tuple/get-long (.getValue %))) @(store/scan-index *db* "refcount_index" ::store/by-group (tuple/all-of ["refcount"]) nil {::store/list? true}))))))
+  (is (= #{"p1" "p2" "p4"}
+         (into #{} (map #(second (.getKey %))
+                        @(store/scan-index *db* "refcount_index" ::store/by-group
+                                           (tuple/all-of ["zero"]) nil {::store/list? true})))))
+  (is (= {"bucket" 1}
+         (scan-refcounting-index (tuple/all-of ["refcount"])))))
